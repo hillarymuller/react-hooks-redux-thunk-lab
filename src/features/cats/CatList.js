1a85f9c7bf811ffe1b57bff0a9c88fbaf05c89a8
Hillary Muller <hillaryaward@gmail.com>
2022-12-14 12:07:28 -0600

    commit tree be4332653a126215ed839c6fd19d5520c79fad9e

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,15 @@
-// write your CatList component here
+import React from 'react';
+
+function CatList({ cats = [] }) {
+    const catPics = cats.map((cat) => {
+        return (<img key={cat.id} alt="cat" src={cat.url}></img>)
+    });
+    console.log(catPics);
+    return (
+        <div>
+        {catPics}
+        </div>
+    )
+}
+
+export default CatList;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import catsReducer from "./features/cats/catsSlice"
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(catsReducer, composedEnhancer);
+
+ReactDOM.render(
+    <Provider store={store}>
+       <App />
+       </Provider>,
+        document.getElementById("root"));
